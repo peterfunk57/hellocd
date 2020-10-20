361d93358083887a516767049b704a876784e271
Pedro Peña <pedro_angel9@hotmail.com>
2020-10-20 13:52:28 -0500

    se agrega java app

---
+++ src/main/java/com/example/HelloJavaApp/HelloJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloJavaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HelloJavaApp/HelloJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloJavaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
